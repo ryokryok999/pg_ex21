64aaeb78db441838e662fcce642a0ba98b555940
ryokryok <ryokryok@192.168.3.8>
2017-08-19 14:33:31 +0900

    8/19 レッツ・リファクタリング

--- src/BillingCaluculate.java
+++ src/BillingCaluculate.java
@@ -12,10 +12,21 @@ public class BillingCaluculate {
 
 	public static final int MAX_KEIYAKUSYA_NUM = 1000; // 適当に1000
 
+	public static final String INPUT_FILE = "record.log";
+	public static final String OUTPUT_FILE = "invoice.dat";
+
+	public static final char INPUT_FIRST_CHAR_KEIYAKUSYA = '1';
+	public static final char INPUT_FIRST_CHAR_KANYU_SERVICE = '2';
+	public static final char INPUT_FIRST_CHAR_TSUUWA_KIROKU = '5';
+	public static final char INPUT_FIRST_CHAR_KUGIRI = '9';
+
+	public static final String SERVICE_KAZOKUWARI = "C1";
+	public static final String SERVICE_HIRUTOKUWARI = "E1";
+
 	public static void main(String[] args) {
 		try{
 			Keiyakusya k[] = new Keiyakusya[MAX_KEIYAKUSYA_NUM];
-			File inputFile = new File("record.log");
+			File inputFile = new File(INPUT_FILE);
 			BufferedReader br = new BufferedReader(new FileReader(inputFile));
 			String line = br.readLine();
 
@@ -24,25 +35,25 @@ public class BillingCaluculate {
 				String[] lineWords = line.split("[\\s]+");
 
 				switch (firstChar){
-				case '1':
+				case INPUT_FIRST_CHAR_KEIYAKUSYA:
 					k[Keiyakusya.getCount()] = new Keiyakusya();
 					k[Keiyakusya.getCount()-1].setKeiyakuNo(lineWords[1]);
 					break;
-				case '2':
+				case INPUT_FIRST_CHAR_KANYU_SERVICE:
 					switch (lineWords[1]){
-					case "C1":
-						k[Keiyakusya.getCount()-1].setC1();
-						k[Keiyakusya.getCount()-1].setC1No(lineWords[2]);
+					case SERVICE_KAZOKUWARI:
+						k[Keiyakusya.getCount()-1].setServiceKazokuwari();
+						k[Keiyakusya.getCount()-1].setServiceKazokuwariNo(lineWords[2]);
 						break;
-					case "E1":
-						k[Keiyakusya.getCount()-1].setE1();
+					case SERVICE_HIRUTOKUWARI:
+						k[Keiyakusya.getCount()-1].setServiceHirutokuwari();
 						break;
 					}
 					break;
-				case '5':
+				case INPUT_FIRST_CHAR_TSUUWA_KIROKU:
 					k[Keiyakusya.getCount()-1].setTsuuwa(lineWords[1], lineWords[2], lineWords[3], lineWords[4]);
 					break;
-				case '9':
+				case INPUT_FIRST_CHAR_KUGIRI:
 					k[Keiyakusya.getCount()-1].calcKihonRyokin();
 					k[Keiyakusya.getCount()-1].calcTsuuwaRyokin();
 					break;
@@ -53,11 +64,11 @@ public class BillingCaluculate {
 			br.close();
 
 			try{
-				File outputFile = new File("invoice.dat");
+				File outputFile = new File(OUTPUT_FILE);
 				PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
 
 				for (int i = 0; i < Keiyakusya.getCount(); i++){
-					System.out.println(k[i]); //本来はいらない
+					System.out.println(k[i]); //テストのため、本来はいらない
 					pw.println(k[i]);
 				}
 				pw.close();
@@ -74,15 +85,39 @@ public class BillingCaluculate {
 }
 
 class Keiyakusya {
+	public static final int MAX_SERVICE_KAZOKUWARI_NO = 2;
+	public static final int MAX_TSUUWA = 1000; // 適当に1000
+
+	public static final int KIHON_RYOKIN = 1000;
+	public static final int KIHON_RYOKIN_SERVICE_KAZOKUWARI = 100;
+	public static final int KIHON_RYOKIN_SERVICE_HIRUTOKUWARI = 200;
+	public static final int TANI_RYOKIN = 20;
+
+	public static final int TANI_RYOKIN_WARIBIKI_SERVICE_HIRUTOKUWARI = 5;
+	public static final double TANI_RYOKIN_WARIBIKIRITSU_SERVICE_HIRUTOKUWARI = 0.5;
+
+	public static final int START_HOUR_SERVICE_HIRUTOKUWARI = 8;
+	public static final int START_MIN_SERVICE_HIRUTOKUWARI = 0;
+	public static final int END_HOUR_SERVICE_HIRUTOKUWARI = 17;
+	public static final int END_MIN_SERVICE_HIRUTOKUWARI = 59;
+
+	public static final char OUTPUT_FIRST_CHAR_KEIYAKUSYA = '1';
+	public static final char OUTPUT_FIRST_CHAR_KINHON_RYOKIN = '5';
+	public static final char OUTPUT_FIRST_CHAR_TSUUWA_RYOKIN = '7';
+	public static final char OUTPUT_FIRST_CHAR_KUGIRI = '9';
+	public static final String OUTPUT_KUGIRI = "====================";
+
 	private static int count = 0;
+
 	private String keiyakuNo;
-	private boolean c1;
-	private boolean e1;
-	private String[] c1No = new String[2];
-	private int c1NoIndex = 0;
-	private String[][] tsuuwa = new String[1000][5];
+	private boolean serviceKazokuwari;
+	private boolean serviceHirutokuwari;
+	private String[] serviceKazokuwariNo = new String[MAX_SERVICE_KAZOKUWARI_NO];
+	private int serviceKazokuwariNoIndex = 0;
+	private String[][] tsuuwa = new String[MAX_TSUUWA][5];
+
 	private int tsuuwaIndex = 0;
-	private int kihonRyokin = 0;
+	private int kihonRyokin;
 	private int tsuuwaRyokin = 0;
 	private int taniRyokin;
 
@@ -91,25 +126,21 @@ class Keiyakusya {
 	}
 
 	public void calcKihonRyokin(){
-		kihonRyokin += 1000;
-		if(c1){
-			kihonRyokin += 100;
+		kihonRyokin = KIHON_RYOKIN;
+		if(serviceKazokuwari){
+			kihonRyokin += KIHON_RYOKIN_SERVICE_KAZOKUWARI;
 		}
-		if(e1){
-			kihonRyokin += 200;
+		if(serviceHirutokuwari){
+			kihonRyokin += KIHON_RYOKIN_SERVICE_HIRUTOKUWARI;
 		}
 	}
 
 	public void calcTsuuwaRyokin(){
-
-
 		for (int i = 0; i < tsuuwaIndex; i++){
-			taniRyokin = 20;
-			int time = Integer.parseInt(tsuuwa[i][2]);
+			taniRyokin = TANI_RYOKIN;
+			int minutes = Integer.parseInt(tsuuwa[i][2]);
 
-			if(e1){
-				//System.out.println("tsuuwa["+i+"][3]=" + tsuuwa[i][3]);
-				//System.out.println("c1No["+j+"]=" + c1No[j]);
+			if(serviceHirutokuwari){
 				String[] yyyymmdd = tsuuwa[i][0].split("/");
 				String[] hhmm = tsuuwa[i][1].split(":");
 				int year = Integer.parseInt(yyyymmdd[0]);
@@ -117,53 +148,32 @@ class Keiyakusya {
 				int day = Integer.parseInt(yyyymmdd[2]);
 				int hour = Integer.parseInt(hhmm[0]);
 				int minute = Integer.parseInt(hhmm[1]);
+
 				Calendar cal = Calendar.getInstance();
 				cal.set(year,month,day,hour,minute);
 				Calendar calBegin = Calendar.getInstance();
-				calBegin.set(year,month,day,8,0);
+				calBegin.set(year,month,day,START_HOUR_SERVICE_HIRUTOKUWARI,START_MIN_SERVICE_HIRUTOKUWARI);
 				Calendar calEnd = Calendar.getInstance();
-				calEnd.set(year,month,day,17,59);
-
-				/*
-				System.out.println("#########");
-				System.out.println(year);
-				System.out.println(month);
-				System.out.println(day);
-				System.out.println(hour);
-				System.out.println(minute);
-
-				System.out.println(cal);
-				System.out.println(calBegin);
-				System.out.println(calEnd);
-				System.out.println(cal.compareTo(calBegin));
-				System.out.println(cal.compareTo(calEnd));
-				*/
+				calEnd.set(year,month,day,END_HOUR_SERVICE_HIRUTOKUWARI,END_MIN_SERVICE_HIRUTOKUWARI);
 
 				if ((cal.compareTo(calBegin) == 1 || cal.compareTo(calBegin) == 0) &&
 						(cal.compareTo(calEnd) == -1 || cal.compareTo(calEnd) == 0)){
-					taniRyokin -= 5;
-					//System.out.println(taniRyokin);
+					taniRyokin -= TANI_RYOKIN_WARIBIKI_SERVICE_HIRUTOKUWARI;
 				}
 			}
 
-			if(c1){
-				//System.out.println("c1マッチ！");
-				for (int j = 0; j < c1NoIndex; j++){
-					if(tsuuwa[i][3].equals(c1No[j])){
-						taniRyokin /= 2;
+			if(serviceKazokuwari){
+				for (int j = 0; j < serviceKazokuwariNoIndex; j++){
+					if(tsuuwa[i][3].equals(serviceKazokuwariNo[j])){
+						taniRyokin *= TANI_RYOKIN_WARIBIKIRITSU_SERVICE_HIRUTOKUWARI;
 					}
 				}
 			}
-
-			tsuuwaRyokin += taniRyokin * time;
-
-			//System.out.println("====");
-			//System.out.println(taniRyokin);
-			//System.out.println(time);
+			tsuuwaRyokin += taniRyokin * minutes;
 		}
-		//System.out.println("**********************");
 	}
 
+	// ここの多次元配列はイケてない。。。
 	public void setTsuuwa(String date, String time, String minutes, String num){
 		tsuuwa[tsuuwaIndex][0] = date;
 		tsuuwa[tsuuwaIndex][1] = time;
@@ -180,48 +190,25 @@ class Keiyakusya {
 		keiyakuNo = newKeiyakuNo;
 	}
 
-	public void setC1(){
-		c1 = true;
+	public void setServiceKazokuwari(){
+		serviceKazokuwari = true;
 	}
 
-	public void setE1(){
-		e1 = true;
+	public void setServiceHirutokuwari(){
+		serviceHirutokuwari = true;
 	}
 
-	public void setC1No(String newC1No){
-		c1No[c1NoIndex] = newC1No;
-		c1NoIndex++;
+	public void setServiceKazokuwariNo(String newServiceKazokuwariNo){
+		serviceKazokuwariNo[serviceKazokuwariNoIndex] = newServiceKazokuwariNo;
+		serviceKazokuwariNoIndex++;
 	}
 
 	public String toString(){
 
-		return "1 " + keiyakuNo + (char)0x0A
-				+ "5 " + kihonRyokin + (char)0x0A
-				+ "7 " + tsuuwaRyokin + (char)0x0A
-				+ "9 ====================";
-
-		/*
-		for (int i = 0; i < tsuuwaIndex; i++){
-			System.out.println(tsuuwa[i][0]);
-			System.out.println(tsuuwa[i][1]);
-			System.out.println(tsuuwa[i][2]);
-			System.out.println(tsuuwa[i][3]);
-		}
-
-		return "契約番号=" + keiyakuNo
-				+ " C1=" + c1
-				+ " C1No[0]=" + c1No[0]
-				+ " C1No[1]=" + c1No[1]
-				+ " E1=" + e1
-				+ " 基本料金=" + kihonRyokin
-				+ " 通話料金=" + tsuuwaRyokin;
-		*/
+		return OUTPUT_FIRST_CHAR_KEIYAKUSYA + " " + keiyakuNo + (char)0x0A
+				+ OUTPUT_FIRST_CHAR_KINHON_RYOKIN + " " + kihonRyokin + (char)0x0A
+				+ OUTPUT_FIRST_CHAR_TSUUWA_RYOKIN + " " + tsuuwaRyokin + (char)0x0A
+				+ OUTPUT_FIRST_CHAR_KUGIRI + " " + OUTPUT_KUGIRI;
 	}
 
 }
-
-
-
-
-
-
